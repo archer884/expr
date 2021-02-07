58f130646eaaae12097663d8778b9803f2b331e4
J/A <archer884@gmail.com>
2021-02-07 14:26:26 -0600

    Update error impl; update crates

--- src/error.rs
+++ src/error.rs
@@ -1,9 +1,12 @@
 use std::num::ParseIntError;
+use std::io;
 
-#[derive(Clone, Debug, thiserror::Error)]
+#[derive(Debug, thiserror::Error)]
 pub enum Error {
     #[error("Unable to parse expression: {0}")]
     BadExpression(String),
     #[error("Bad integer: {0}; {1}")]
     BadInteger(String, ParseIntError),
+    #[error(transparent)]
+    IoError(#[from] io::Error),
 }
